61a542aea9cac10f074b9f2e0e12fc6a0eff3cd0
Fernando Rocha <fernandorsl@gmail.com>
2016-10-17 08:09:25 -0500

    testing package manager

---
+++ spotify-api/Package.swift
@@ -0,0 +1,14 @@
+//
+//  Package.swift
+//  spotify-api
+//
+//  Created by Fernando Rocha Silva on 10/10/16.
+//  Copyright © 2016 Fernando Rocha Silva. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "SitePointSPM",
+    dependencies: []
+)
